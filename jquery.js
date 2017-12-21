f66e40fe1378327eed4f40c3c94f7d609cfda9c1
Nguyen Hanh <ndhanh96@gmail.com>
2017-12-22 06:18:19 +0700

    sorting some function

--- jquery.js
+++ jquery.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
    var box8 = true;
    var box9 = true;
 
-   var user_click;
+   var user_clicked = false;
    var tie_game = false;
 
    var arrToWin = [
@@ -37,18 +37,38 @@ $(document).ready(function() {
    $(".box-tick").hide();
    $("#chooseO").click(function() {
       user = 'O';
-      user_click = true;
       computer = 'X';
       $(".opening").hide();
       $(".box-tick").show();
    });
    $("#chooseX").click(function() {
       user = 'X';
-      user_click = true;
       computer = 'O';
       $(".opening").hide();
       $(".box-tick").show();
    });
+
+   function whatBox(numb) {
+      if(numb == 1) {
+         box1 = false;
+      } else if (numb == 2) {
+         box2 = false;
+      } else if (numb == 3) {
+         box3 = false;
+      } else if (numb == 4) {
+         box4 = false;
+      } else if (numb == 5) {
+         box5 = false;
+      } else if (numb == 6) {
+         box6 = false;
+      } else if (numb == 7) {
+         box7 = false;
+      } else if (numb == 8) {
+         box8 = false;
+      } else {
+         box9 = false;
+      }
+   }
 
    function checkTieGame() {
       if(arrOfPlayer.length == 5) {
@@ -65,47 +85,50 @@ $(document).ready(function() {
       }
    }
 
-   function canUserClick(where, box) {
-      if (box) {
+   function canUserClick(where, box, numb) {
+      if (box == true && user_clicked == false) {
+         whatBox(numb);
          $(where).text(user);
+         arrOfPlayer.push(numb);
          checkTieGame();
+         user_clicked = true;
+         compClick();
       }
    }
 
    function checkPlayerMove(where, numb) {
-      $(where).text(computer);
-      box = false;
+      whatBox(numb);
+      setTimeout(function() {
+         $(where).text(computer);
+      },1000);
+      user_clicked = false;
+
       checkArr(numb);
    }
 
    function compClick() {
-      var player = arrOfPlayer[arrOfPlayer.length - 1];
-      setTimeout(function() {
-         if (arrOfPlayer[0] == 1) {
-            checkPlayerMove("#content-box5",5);
-            box5 = false;
-            if (arrOfPlayer[1] == 2) {
-               checkPlayerMove("#content-box3",3);
-               box3 = false;
-               if(arrOfPlayer[2] == 4 || arrOfPlayer[2] == 6 || arrOfPlayer[2] == 9 || arrOfPlayer[2] == 8) {
-                  checkPlayerMove("#content-box7",7);
-                  box7 = false;
-               } else if (arrOfPlayer[2] == 7) {
-                  checkPlayerMove("#content-box4",4);
-                  box4 = false;
-                  if(arrOfPlayer[3] == 8 || arrOfPlayer[3] == 9) {
-                     checkPlayerMove("#content-box6",6);
-                     box6 = false;
-                  } else if (arrOfPlayer[3] == 6) {
-                     checkPlayerMove("#content-box9",9);
-                     box9 = false;
-                  }
+      // var player = arrOfPlayer[arrOfPlayer.length - 1];
+      // user_clicked = false;
+      if (arrOfPlayer[0] == 1) {
+         checkPlayerMove("#content-box5",5);
+         if (arrOfPlayer[1] == 2) {
+            checkPlayerMove("#content-box3",3);
+            if(arrOfPlayer[2] == 4 || arrOfPlayer[2] == 6 || arrOfPlayer[2] == 9 || arrOfPlayer[2] == 8) {
+               checkPlayerMove("#content-box7",7);
+            } else if (arrOfPlayer[2] == 7) {
+               checkPlayerMove("#content-box4",4);
+               if(arrOfPlayer[3] == 8 || arrOfPlayer[3] == 9) {
+                  checkPlayerMove("#content-box6",6);
+               } else if (arrOfPlayer[3] == 6) {
+                  checkPlayerMove("#content-box9",9);
                }
             }
          }
-
+      }
+      setTimeout(function() {
          calculate();
-      }, 1000);
+      },1000)
+
 
    }
 
@@ -115,7 +138,6 @@ $(document).ready(function() {
          return a - b;
       });
       arrOfCompCheck = JSON.stringify(arrOfComp);
-      // alert(arrOfComp);
       if (newArr.includes(arrOfCompCheck)) {
          setTimeout(function() {
             alert("You Lose");
@@ -127,66 +149,30 @@ $(document).ready(function() {
 
 
    $("#box1").click(function() {
-      arrOfPlayer.push(1);
-      canUserClick("#content-box1", box1);
-      box1 = false;
-      
-      compClick();
+      canUserClick("#content-box1", box1,1);
    });
    $("#box2").click(function() {
-      arrOfPlayer.push(2);
-      canUserClick("#content-box2", box2);
-      
-      compClick();
-      box2 = false;
+      canUserClick("#content-box2", box2,2);
    });
    $("#box3").click(function() {
-      arrOfPlayer.push(3);
-      canUserClick("#content-box3", box3);
-      
-      compClick();
-      box3 = false;
+      canUserClick("#content-box3", box3,3);
    });
    $("#box4").click(function() {
-      arrOfPlayer.push(4);
-      canUserClick("#content-box4", box4);
-      
-      compClick();
-      box4 = false;
+      canUserClick("#content-box4", box4,4);
    });
    $("#box5").click(function() {
-      arrOfPlayer.push(5);
-      canUserClick("#content-box5", box5);
-      
-      compClick();
-      box5 = false;
+      canUserClick("#content-box5", box5,5);
    });
    $("#box6").click(function() {
-      arrOfPlayer.push(6);
-      canUserClick("#content-box6", box6);
-      
-      compClick();
-      box6 = false;
+      canUserClick("#content-box6", box6,6);
    });
    $("#box7").click(function() {
-      arrOfPlayer.push(7);
-      canUserClick("#content-box7", box7);
-      
-      compClick();
-      box7 = false;
+      canUserClick("#content-box7", box7,7);
    });
    $("#box8").click(function() {
-      arrOfPlayer.push(8);
-      canUserClick("#content-box8", box8);
-      
-      compClick();
-      box8 = false;
+      canUserClick("#content-box8", box8,8);
    });
    $("#box9").click(function() {
-      arrOfPlayer.push(9);
-      canUserClick("#content-box9", box9);
-      
-      compClick();
-      box9 = false;
+      canUserClick("#content-box9", box9,9);
    });
 });
